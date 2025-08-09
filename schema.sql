a2767a40ecc8acd3bc714024f86032a00f833fb7
alexgaulista-ux <alexgaulista@gmail.com>
2025-08-09 16:34:06 -0300

    Add files via upload

--- schema.sql
+++ schema.sql
@@ -4,3 +4,14 @@ CREATE TABLE registros (
   sentimento TEXT NOT NULL,
   data TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
+
+
+-- Tabela de usuários para login
+CREATE TABLE usuarios (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    usuario TEXT NOT NULL UNIQUE,
+    senha TEXT NOT NULL
+);
+
+-- Usuário padrão (alterar depois)
+INSERT INTO usuarios (usuario, senha) VALUES ('admin', 'Duroline001*');
